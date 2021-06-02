1aaf604227dcea53fa0f125847f2fd0d3ac4d92e
Aluno <stefanie.costa@bandtec.com.br>
2021-06-02 12:50:38 -0300

    exercicios de algoritimos

---
+++ algoritimos/validarcnpj/index.js
@@ -0,0 +1,67 @@
+function validarCNPJ(cnpj) {
+ 
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+ 
+    if(cnpj == '') return false;
+     
+    if (cnpj.length != 14)
+        return false;
+ 
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999")
+        return false;
+         
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+         
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
+    
+}
+
+//Na linha 3, eliminamos todos os caracteres não númericos do CNPJ passado como parâmetro, 
+//considerando que quem invocou a função pode ter utilizado-se de uma possível máscara no campo.
+
+//A instrução if da linha 7 verifica se a quantidade de numerais da string
+// (considerando-a já limpa) é igual a quantidade correta - 14.
+
+//As linha 10 à 21 checam se a string é formado apenas por valores iguais. 
+//Esta verificação é necessária uma vez que se aplicarmos o algoritmo do CNPJ.
+
+//As linha 23 à 49 checam os dois dígitos verificadores, 
+//verificando sua validade de acordo com o algoritmo do CNPJ. Caso negativo, a validação retorna false encerrando a função.
+
+//Caso o algoritmo alcance a linha 51 temos um CNPJ válido e o valor true é retornado.
